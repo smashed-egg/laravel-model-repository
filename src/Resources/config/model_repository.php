725068bed48e25b7323fbc78bf10079abb1457be
Tom Ellis <tellishtc@gmail.com>
2023-06-23 15:42:55 +0100

    Add missing files

---
+++ src/Resources/config/model_repository.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    /**
+     * The base Repository Class to use for all Repositories generated via cli
+     */
+    'base_repository' => \SmashedEgg\LaravelModelRepository\Repository\Repository::class,
+];
---
+++ tests/CustomRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SmashedEgg\LaravelModelRepository\Tests;
+
+use SmashedEgg\LaravelModelRepository\Repository\Repository;
+
+class CustomRepository extends Repository
+{
+
+}
+
